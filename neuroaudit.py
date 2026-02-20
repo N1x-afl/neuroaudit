7ec1836c9f54994c90dba1bf20abebf76d32f16a
Felipe Loaiza <feloaiza@outlook.com>
2026-02-20 20:42:47 -0300

    Update
    Modificamso hash

--- neuroaudit.py
+++ neuroaudit.py
@@ -13,7 +13,7 @@ VERSION = "4.5 Hardened"
 SYSTEM_NAME = "NEUROAUDIT - Security & IT Suite"
 DEVELOPER = "Felipe Soluciones IT"
 # Este es el Hash que vamos a validar al final
-OFFICIAL_HASH = "da3cca4fa30ad95731c33904c4863571eaaef183c14556ef9252ff55b61d0ad9"
+OFFICIAL_HASH = "5dcf7e7e4c63e11516ef9303525a1e7b7f95b27c242103385a9b0839001f8672"
 
 class Colors:
     HEADER = '\033[95m'
